11b41b95657d020a1b02518f0f2abd6b2df660d0
Krish Koria <krishkoria2004@gmail.com>
2024-10-02 11:33:20 +0530

    Refactor page layout and add sign-in and sign-up routes
    - Refactored the Home component in page.tsx to include a CSS class for styling.
    - Added sign-in and sign-up routes in separate page components.
    - Updated dependencies in package.json to include "@clerk/nextjs" version 5.6.4.
    - Created a middleware in middleware.ts to protect private routes.
    - Modified the Navbar component in navbar.tsx to include a user button when signed in.
    - Updated the RootLayout component in layout.tsx to include the ClerkProvider wrapper.

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,14 +1,20 @@
 import Image from "next/image";
 import Link from "next/link";
 import logo from "@/assets/logo.png";
+import { SignedIn, UserButton } from "@clerk/nextjs";
 export default function Navbar() {
   return (
     <div className="">
-      <header>
-        <Link href="/">
-          <Image src={logo} alt="logo" />
+      <header className="flex items-center justify-between">
+        <Link href="/" className="flex items-center gap-2 font-bold">
+          <Image src={logo} alt="logo" width={"32"} height={"32"} />
           <span>NextAI</span>
         </Link>
+        <div className="">
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
+        </div>
       </header>
     </div>
   );
