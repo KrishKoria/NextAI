5488a51b0267b2bccc71dab7e100281aa60e6b33
Krish Koria <krishkoria2004@gmail.com>
2024-10-12 00:42:39 +0530

    added cloudinary upload functionality

--- components/chatPrompt.tsx
+++ components/chatPrompt.tsx
@@ -1,39 +1,71 @@
 "use client";
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { Label } from "./ui/label";
 import { Input } from "./ui/input";
-import attachment from "@/assets/attachment.png";
-import Image from "next/image";
 import { Button } from "./ui/button";
+import { CldUploadWidget } from "next-cloudinary";
+import Image from "next/image";
+import attachment from "@/assets/attachment.png";
 import arrow from "@/assets/arrow.png";
+
 export default function ChatPrompt() {
+  const [message, setMessage] = useState("");
   const endRef = useRef<HTMLDivElement>(null);
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const handleSuccess = (result: any) => {
+    console.log(result.info.secure_url);
+  };
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    // Handle sending the message here
+    console.log("Sending message:", message);
+    setMessage("");
+  };
+
   useEffect(() => {
     if (endRef.current) {
       endRef.current.scrollIntoView({ behavior: "smooth" });
     }
   }, []);
+
   return (
     <>
       <div className="pb-[50px]" ref={endRef} />
-      <form className="absolute bottom-0 flex w-1/2 items-center rounded-full bg-[#2c2937] p-2.5">
-        <Label
-          htmlFor="picture"
-          className="flex cursor-pointer items-center justify-center rounded-[50%] border-none bg-[#605e68] p-2.5"
+      <form
+        onSubmit={handleSubmit}
+        className="absolute bottom-0 left-1/4 flex w-1/2 items-center rounded-full bg-[#2c2937] p-2.5"
+      >
+        <CldUploadWidget
+          uploadPreset="myuploadpreset"
+          onSuccess={handleSuccess}
+          options={{
+            folder: "NextAI",
+            resourceType: "image",
+          }}
         >
-          <Image src={attachment} alt="attachment" width={16} height={16} />
-        </Label>
-        <Input
-          id="picture"
-          type="file"
-          className="hidden flex-1 border-none bg-transparent p-5 text-[#ececec] outline-none"
-        />
+          {({ open }) => (
+            <Label
+              htmlFor="picture"
+              className="flex cursor-pointer items-center justify-center rounded-full border-none bg-[#605e68] p-2.5"
+              onClick={() => open()}
+            >
+              <Image src={attachment} alt="attachment" width={16} height={16} />
+            </Label>
+          )}
+        </CldUploadWidget>
         <Input
           type="text"
           placeholder="Ask Anything...."
           className="flex-1 border-none bg-transparent p-5 text-[#ececec] outline-none"
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
         />
-        <Button className="flex cursor-pointer items-center justify-center rounded-[50%] border-[none] bg-[#605e68] p-2.5">
+        <Button
+          type="submit"
+          className="flex cursor-pointer items-center justify-center rounded-full border-none bg-[#605e68] p-2.5"
+        >
           <Image src={arrow} alt="arrow" width={16} height={16} />
         </Button>
       </form>
